35c5756b9ffe5c267e63f3e7e2971e5416dbe17d
User <User@DESKTOP-G1J19HC>
2024-03-06 11:54:24 +0200

    Add project files.

---
+++ WaiterChefBoss/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WaiterChefBoss.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
